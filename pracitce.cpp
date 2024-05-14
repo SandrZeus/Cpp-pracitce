e14f60a0a90a1f9b5783986c17168b4a5177f422
SandrZeus <sandro.bandro10@gmail.com>
2024-05-14 23:12:23 +0200

    value vs reference

--- pracitce.cpp
+++ pracitce.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+void swap(std::string x, std::string y);
 
 int main() {
 
-    std::cout << "Game over";
+    std::string x = "Kool-Aid";
+    std::string y = "Water";
+    std::string temp;
+
+    swap(x, y);
+
+    std::cout << "X: " << &x << '\n';
+    std::cout << "Y: " << &y << '\n';
 
     return 0;
+}
+void swap(std::string x, std::string y){
+    std::cout << "X: " << &x << '\n';
+    std::cout << "Y: " << &y << '\n';
 }
